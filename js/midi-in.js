a9708017bd643dbf9888f865d0a93ed8a7cc6e64
Solra Bizna <solra@bizna.name>
2017-08-29 16:08:20 -0600

    These aren't being used anymore

--- js/midi-in.js
+++ js/midi-in.js
@@ -193,13 +193,3 @@
 		midiBox.remove();
 	}
 }
-
-let noteOff = function( noteNumber ){
-	app.handleKeyOff(noteNumber);
-};
-let noteOn = function( noteNumber, velocity){
-	app.handleKeyOn(noteNumber);
-};
-let controller = function( noteNumber, velocity){};
-let pitchWheel = function( pitch ){};
-let polyPressure = function(noteNumber,velocity){};
